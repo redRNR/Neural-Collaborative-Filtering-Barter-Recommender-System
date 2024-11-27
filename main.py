b1f4fee063b60185f785352bcb003d5d18b06efd
Azra Park <azrajamesp@gmail.com>
2024-11-27 12:38:53 -0500

    Revert "Hyperparameter tuning"
    This reverts commit 598628145193551cdf9024a801c7bc85aa2f1af0.

--- main.py
+++ main.py
@@ -18,24 +18,21 @@ train_data, num_users, num_items, test_data, social_adj, C = load_data(
 print('Data loaded')
 
 # Instantiate the NCF model
-for embedding_dims in [40, 50, 60, 80]:
-    model = NCF(num_users, num_items, embedding_dim=embedding_dims)
-    print(f'Model instantiated with embedding_dims={embedding_dims}')
-    print('Model instantiated')
+embedding_dim = 64
+model = NCF(num_users, num_items, embedding_dim=embedding_dim)
+print('Model instantiated')
 
-    # Training using Neural Collaborative Filtering
-    epochs = 500
-    batch_size = 256
-    learning_rate = 0.001
-    batch_sizes = [128, 256, 512, 1024]
-    for batch_size in batch_sizes:
-        print(f'Training with batch_size={batch_size}')
-        train_ncf(model, train_data, social_adj, num_users, num_items, epochs=epochs, batch_size=batch_size, lr=learning_rate)
-        print('Training completed')
+# Training using Neural Collaborative Filtering
+epochs = 10
+batch_size = 1024
+learning_rate = 0.001
 
-        # Prepare user-item set for evaluation
-        user_item_set = set(zip(train_data[:, 1].numpy(), train_data[:, 2].numpy()))
+train_ncf(model, train_data, social_adj, num_users, num_items, epochs=epochs, batch_size=batch_size, lr=learning_rate)
+print('Training completed')
 
-        # Evaluate the model
-        from evaluate import evaluate
-        evaluate(model, test_data, num_users, num_items, social_adj, user_item_set)
+# Prepare user-item set for evaluation
+user_item_set = set(zip(train_data[:, 1].numpy(), train_data[:, 2].numpy()))
+
+# Evaluate the model
+from evaluate import evaluate
+evaluate(model, test_data, num_users, num_items, social_adj, user_item_set)
